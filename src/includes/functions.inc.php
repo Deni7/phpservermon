47d03092737a774d6070c66cd524b8c6abb628be
Denis Stepanov <denis@stryi.com.ua>
2021-04-29 13:09:05 +0300

    correct ?->&

--- src/includes/functions.inc.php
+++ src/includes/functions.inc.php
@@ -1049,7 +1049,7 @@ namespace {
         {
             if (!empty($this->token) && !empty($this->user) && !empty($this->message)) {
                 $this->url = 'https://telegram.swrz.com.ua/sendtg.php?botToken=' . urlencode($this->token) .
-                    '&type_message=sendmessage?chat_id=' . urlencode($this->user) . '&text_message=' .
+                    '&type_message=sendmessage&chat_id=' . urlencode($this->user) . '&text_message=' .
                     urlencode($this->message) . '&parse_mode=HTML&disable_web_page_preview=True';
             }
             return $this->sendurl();
